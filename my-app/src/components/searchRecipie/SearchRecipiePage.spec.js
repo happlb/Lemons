3e590e52074f9be2deac5c25847883faed0dde62
Lucy Happ <lucyhapp@northwesternmutual.com>
2020-07-07 11:56:17 -0500

    testing

--- my-app/src/components/searchRecipie/SearchRecipiePage.spec.js
+++ my-app/src/components/searchRecipie/SearchRecipiePage.spec.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import { mount } from "enzyme";
-import SearchRecipiePage from "./SearchRecipiePage";
-import { configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-import { Provider } from "react-redux";
-import configureStore from "redux-mock-store";
-=======
 import React from 'react'
 import { mount } from 'enzyme'
 import SearchRecipiePage from './SearchRecipiePage'
@@ -15,53 +6,67 @@ import Adapter from 'enzyme-adapter-react-16'
 import { Provider } from 'react-redux'
 import configureStore from 'redux-mock-store'
 
-import { wrap } from 'module'
->>>>>>> master
+configure({ adapter: new Adapter() })
 
-configure({ adapter: new Adapter() });
-
-function renderSearchRecipiePage(args) {
+function renderSearchRecipiePage (args) {
   const defaultProps = {
     vegan: false,
     veg: false,
     ingredents: [],
-    recipies: [],
-  };
-  const mockStore = configureStore();
-  const props = { ...defaultProps, ...args };
-  const store = mockStore(props);
+    recipies: []
+  }
+  const mockStore = configureStore()
+  const props = { ...defaultProps, ...args }
+  const store = mockStore(props)
 
   return mount(
     <Provider store={store}>
       <SearchRecipiePage />
     </Provider>
-  );
+  )
 }
-describe("test Added ingredient list renders", () => {
-  it("renders init", () => {
-    const wrapper = renderSearchRecipiePage();
-    expect(wrapper.find("div.AddedIngedents").text()).toEqual("");
-  });
-  it("test added ingredent is displayed", () => {
-    const wrapper = renderSearchRecipiePage({ ingredents: ["apple"] });
-    expect(wrapper.find("div.AddedIngedents").text()).toEqual("apple");
-  });
-  it("test added ingredent can be deleted", () => {
-    const wrapper = renderSearchRecipiePage({ ingredents: ["apple", "pear"] });
+describe('test added ingredient list renders correctly', () => {
+  it('renders init', () => {
+    const wrapper = renderSearchRecipiePage()
+    expect(wrapper.find('div.AddedIngedents').text()).toEqual('')
+  })
+
+  it('test added ingredent is displayed', () => {
+    const wrapper = renderSearchRecipiePage({ ingredents: ['apple'] })
+    expect(wrapper.find('div.AddedIngedents').text()).toEqual('apple')
+  })
+
+  it('test added ingredents is displayedd', () => {
+    const wrapper = renderSearchRecipiePage({ ingredents: ['apple', 'pear'] })
     expect(
       wrapper
-        .find("div.AddedIngedents")
+        .find('div.AddedIngedents')
         .children()
-        .map((cur) => cur.text())
-    ).toEqual(["apple", "pear"]);
-  });
-  it("test added ingredent can be deleted", () => {
-    const wrapper = renderSearchRecipiePage({ ingredents: ["apple", "pear"] });
+        .map(cur => cur.text())
+    ).toEqual(['apple', 'pear'])
+  })
+})
 
-    wrapper
-      .find(".AddedIngedents")
-      .children()
-      .map((cur) => cur.find("input").simulate("click", { target: {} }));
-    expect(wrapper.find("div.AddedIngedents").children().length).toEqual(0);
-  });
-});
+describe('test recipie list renders correctly', () => {
+  xit('renders init', () => {
+    const wrapper = renderSearchRecipiePage({ recipies: [] })
+    expect(wrapper.find('div.AddedRecipies').text()).toEqual('')
+  })
+
+  xit('test added ingredent is displayed', () => {
+    const wrapper = renderSearchRecipiePage({ recipies: ['chicken'] })
+    expect(wrapper.find('div.AddedRecipies').text()).toEqual('chicken')
+  })
+
+  xit('test added ingredents is displayedd', () => {
+    const wrapper = renderSearchRecipiePage({
+      recipies: ['chicken', 'more chicken']
+    })
+    expect(
+      wrapper
+        .find('div.AddedRecipies')
+        .children()
+        .map(cur => cur.text())
+    ).toEqual(['chicken', 'more chicken'])
+  })
+})
